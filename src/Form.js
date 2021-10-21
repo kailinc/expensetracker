df6fdb694d221eed9a2ffced04bd2db01bec580e
kailinc <kailinc@bu.edu>
2021-10-21 15:16:37 +0700

    added handletextchange for form

--- src/Form.js
+++ src/Form.js
@@ -1,16 +1,31 @@
 import React from 'react';
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      caption: '',
+      amount: 0
+    }
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(e) {
+    this.setState({
+      caption: e.target.value
+    })
+  }
+
   render() {
     return(
       <div>
         <h1>Add New Transaction</h1>
         <form>
           <h3>Caption</h3>
-          <input type='text'/>
+          <input type='text' placeholder='Enter Your Caption Here'value={this.state.caption} onChange={this.handleTextChange}/>
           <h3>Amount</h3>
           <p>Negative is expense. Positive is income</p>
-          <input type='number'/>
+          <input type='number' value={this.state.amount}/>
           <input type='button'/>
         </form>
       </div>
