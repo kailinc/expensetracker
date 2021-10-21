69ad6d2b39584247ca44147fe66ce6c68457109a
kailinc <kailinc@bu.edu>
2021-10-21 15:05:07 +0700

    added css for dashboard component

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,12 +5,15 @@ class Dashboard extends React.Component {
     return(
       <div>
         <h2>Your Balance</h2>
-        <h2>${this.props.balance}</h2>
+        {this.props.balance >= 0 ?
+          <h2 className='plus'>${this.props.balance}</h2> :
+          <h2 className='minus'>-${this.props.balance * -1}</h2> }
+
         <div>
           <h3>INCOME</h3>
-          <h3>${this.props.income}</h3>
+          <h3 className='plus'>${this.props.income}</h3>
           <h3>EXPENSE</h3>
-          <h3>${this.props.expense}</h3>
+          <h3 className='minus'>${this.props.expense}</h3>
         </div>
       </div>
     )
