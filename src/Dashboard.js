c450d03f0d8d442b656bc5c61cd00ec8d1987d86
kailinc <kailinc@bu.edu>
2021-10-22 10:33:11 +0700

    added css for header and dashboard component

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,16 +4,20 @@ class Dashboard extends React.Component {
   render() {
     return(
       <div>
-        <h2>Your Balance</h2>
+        <h2>YOUR BALANCE</h2>
         {this.props.balance >= 0 ?
           <h2 className='plus'>${this.props.balance}</h2> :
           <h2 className='minus'>-${this.props.balance * -1}</h2> }
 
-        <div>
-          <h3>INCOME</h3>
-          <h3 className='plus'>${this.props.income}</h3>
-          <h3>EXPENSE</h3>
-          <h3 className='minus'>${this.props.expense}</h3>
+        <div className="summary">
+          <div>
+            <h4>INCOME</h4>
+            <h4 className='plus'>${this.props.income}</h4>
+          </div>
+          <div>
+          <h4>EXPENSE</h4>
+          <h4 className='minus'>${this.props.expense}</h4>
+          </div>
         </div>
       </div>
     )
